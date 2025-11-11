8d7f4ef8233e0bc8a0f7618d64ba821aaa5c7b89
musab <malqarninet@outlook.com>
2025-11-11 16:17:25 +0300

    Excercise 1: delete a software engineer handle unlisted id

--- src/main/java/com/musab/spring_boot/SoftwareEngineerController.java
+++ src/main/java/com/musab/spring_boot/SoftwareEngineerController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,10 +28,20 @@ public class SoftwareEngineerController {
         return softwareEngineerService.getSoftwareEngineers();
     }
 
+    @GetMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
     @PostMapping
     public void addNewSoftwareEngineer(
             @RequestBody SoftwareEngineer softwareEngineer) {
         softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 
+    @DeleteMapping("{id}")
+    public void removeSoftwareEngineerById(
+            @PathVariable Integer id) {
+         softwareEngineerService.removeSoftwareEngineerById(id);
+    }
 }
--- src/main/java/com/musab/spring_boot/SoftwareEngineerService.java
+++ src/main/java/com/musab/spring_boot/SoftwareEngineerService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.musab.spring_boot.exception.ResourceNotFoundException;
+
 @Service // where the bussiness logic written
 public class SoftwareEngineerService {
    private final SoftwareEngineerRepository softwareEngineerRepository;
@@ -17,7 +19,20 @@ public class SoftwareEngineerService {
    }
 
    public void insertSoftwareEngineer(
-      SoftwareEngineer softEngineer){
-      softwareEngineerRepository.save(softEngineer);
+         SoftwareEngineer softwareEngineer) {
+      softwareEngineerRepository.save(softwareEngineer);
+   }
+
+   public SoftwareEngineer getSoftwareEngineerById(Integer id) {
+      return softwareEngineerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Software engineer with id " + id + " not found"));
+   }
+
+   public void removeSoftwareEngineerById(Integer id) {
+      boolean exists = softwareEngineerRepository.existsById(id);
+      if (!exists) {
+         throw new ResourceNotFoundException("Software engineer with id " + id + " not found");
+      } else {
+         softwareEngineerRepository.deleteById(id);
+      }
    }
 }
